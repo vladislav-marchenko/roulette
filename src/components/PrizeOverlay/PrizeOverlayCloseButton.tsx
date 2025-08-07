ff83336f598a2b047422ab0d1d37a3274a35cfea
Владислав <maksimsraud@gmail.com>
2025-08-08 02:34:50 +0300

    refactor: changed prize overlay close button color on hover

--- src/components/PrizeOverlay/PrizeOverlayCloseButton.tsx
+++ src/components/PrizeOverlay/PrizeOverlayCloseButton.tsx
@@ -2,7 +2,7 @@ import { AiOutlineClose } from 'react-icons/ai'
 
 export const PrizeOverlayCloseButton = () => {
   return (
-    <button className='absolute top-4 right-4 z-30 flex cursor-pointer items-center justify-center rounded-full border border-neutral-500/50 bg-neutral-600 p-1 hover:bg-neutral-700'>
+    <button className='absolute top-4 right-4 z-30 flex cursor-pointer items-center justify-center rounded-full border border-neutral-500/50 bg-neutral-600 p-1 transition-colors duration-200 hover:bg-neutral-700'>
       <AiOutlineClose size={20} />
     </button>
   )
