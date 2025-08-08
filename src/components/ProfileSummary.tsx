03e55aca1464d824a378aea037d38bcfac36befe
Владислав <maksimsraud@gmail.com>
2025-08-08 14:48:42 +0300

    fix: header

--- src/components/ProfileSummary.tsx
+++ src/components/ProfileSummary.tsx
@@ -6,7 +6,7 @@ export const ProfileSummary = () => {
     <Link to='/profile' className='flex items-center gap-1'>
       <Image
         src='https://tailwindcss.com/_next/static/media/cover.de1997f7.png'
-        className='w-9 overflow-hidden rounded-full border-2 border-neutral-600/80'
+        className='w-7 overflow-hidden rounded-full border-2 border-neutral-600/80'
       />
       <span className='text-xs font-bold'>42/250 XP</span>
     </Link>
