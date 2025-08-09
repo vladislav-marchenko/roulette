ae8a5ca03902e178d209dfc083cfa726fe17eccc
Владислав <maksimsraud@gmail.com>
2025-08-09 21:29:06 +0300

    fix: minor settings fixes

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -40,7 +40,7 @@ export const Russia = () => {
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <g clip-path='url(#clip0_3037_7707)'>
+      <g clipPath='url(#clip0_3037_7707)'>
         <path
           d='M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z'
           fill='#F0F0F0'
@@ -72,7 +72,7 @@ export const UnitedStates = () => {
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <g clip-path='url(#clip0_3037_29694)'>
+      <g clipPath='url(#clip0_3037_29694)'>
         <path
           d='M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z'
           fill='#F0F0F0'
